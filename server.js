7f3b39588ebb10060b61a4b8a5eef0f2f9af9d88
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-14 02:21:20 +0300

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import { pool } from './config.js'
 import cors from 'cors';
 import dotenv from "dotenv";
-import { pool } from './config.js'
 import users from "./routes/users.js";
 
 const app = express();
